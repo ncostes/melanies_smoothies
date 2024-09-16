3c59ad044b912c0866b8b81ef2316ca9ca4cccee
ncostes <120173109+ncostes@users.noreply.github.com>
2024-09-16 10:25:22 +0200

    Update streamlit_app.py
    adding deubbging codes to unerstand the json() issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.title(":cup_with_straw: Customize Your PERFECT Smoothie! :cup_with_straw:")
 st.write(
-    """Choose the fruits you want in your custom Smoothie!
+    """Choose ALL the fruits you want in your custom Smoothie!
     """)
 
 name_on_order = st.text_input('Name on Smoothie:')
